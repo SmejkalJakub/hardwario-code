bef0b4dbf07e98db7ef0573c63d3367f699ad3dc
Jakub Smejkal <smejkal.jakub99@gmail.com>
2023-01-31 21:16:46 +0100

    Update Create Data Folder Script

--- createDataFolder.sh
+++ createDataFolder.sh
@@ -8,23 +8,28 @@ then
     mv Windows/JLink_Windows_* Windows/data/tower/toolchain/jlink.exe
     mkdir Windows/data/tower/toolchain/SEGGER
     mkdir Windows/data/tower/toolchain/SEGGER/JLink
-    7z x Windows/data/tower/toolchain/jlink.exe -oWindows/data/tower/toolchain/SEGGER/JLink -r
+    7z x Windows/data/tower/toolchain/jlink.exe -oWindows/data/tower/toolchain/SEGGER/JLink -r -y
 
     curl -o Windows/data/tower/toolchain/cmake.zip --ssl-no-revoke -LO "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-windows-x86_64.zip" 
     unzip -o Windows/data/tower/toolchain/cmake.zip -d Windows/data/tower/toolchain/
 
     curl -o Windows/data/tower/toolchain/gcc.zip --ssl-no-revoke -LO "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-mingw-w64-i686-arm-none-eabi.zip" 
-    7z x Windows/data/tower/toolchain/gcc.zip -oWindows/data/tower/toolchain/ -r
+    7z x Windows/data/tower/toolchain/gcc.zip -oWindows/data/tower/toolchain/ -r -y
 
     mkdir Windows/data/tower/toolchain/ninja
     curl -o Windows/data/tower/toolchain/ninja.zip --ssl-no-revoke -LO "https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-win.zip" 
-    7z x Windows/data/tower/toolchain/ninja.zip -oWindows/data/tower/toolchain/ -r
+    7z x Windows/data/tower/toolchain/ninja.zip -oWindows/data/tower/toolchain/ -r -y
 
     mkdir Windows/data/tower/toolchain/git
     curl -o Windows/data/tower/toolchain/git.7z.exe --ssl-no-revoke -LO "https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/PortableGit-2.39.1-64-bit.7z.exe" 
-    7z x Windows/data/tower/toolchain/git.7z.exe -oWindows/data/tower/toolchain/git/ -r
+    7z x Windows/data/tower/toolchain/git.7z.exe -oWindows/data/tower/toolchain/git/ -r -y
 
     mv Windows/data/tower/toolchain/cmake-* Windows/data/tower/toolchain/cmake
     mv Windows/data/tower/toolchain/arm-* Windows/data/tower/toolchain/gcc
     mv Windows/data/tower/toolchain/ninja.exe Windows/data/tower/toolchain/ninja/ninja.exe
+
+    rm -rf Windows/data/tower/toolchain/*.zip
+    rm -rf Windows/data/tower/toolchain/*.7z.exe
+    rm -rf Windows/data/tower/toolchain/*.exe
+    rm -rf Windows/data/tower/toolchain/.gitignore
 fi
